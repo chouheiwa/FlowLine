c658cf686c3f55e1db3f45c08492c56ba17e01b5
chouheiwa <849131492@qq.com>
2025-08-06 21:56:05 +0800

    fix: 更新API基础URL以使用动态主机名和端口

--- web-react/src/services/api.ts
+++ web-react/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { GpuInfo, TaskInfo, CreateTaskRequest, CopyTaskRequest } from '../types'
 
-const API_BASE_URL = `http://localhost:5001/api`
+const API_BASE_URL = `http://${window.location.hostname}:5000/api`
 
 const api = axios.create({
   baseURL: API_BASE_URL,
